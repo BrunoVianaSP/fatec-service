02ad9d1274a17a9cabb950111696206d065dc5c1
Bruno Barbosa <bruno.barbosa@keyrus.com.br>
2018-08-30 14:51:09 -0300

    Schedule activity

--- fatecapp/app/src/main/java/dev/fatecsp/MainActivity.java
+++ fatecapp/app/src/main/java/dev/fatecsp/MainActivity.java
@@ -1,5 +1,6 @@
 package dev.fatecsp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -12,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import dev.fatecsp.schedule.ScheduleActivity;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -80,17 +83,19 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_performance) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_schedule) {
+            Intent i = new Intent(getApplicationContext(), ScheduleActivity.class);
+            startActivity(i);
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_settings) {
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_contact) {
 
         }
 
